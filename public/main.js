4019b0f24ee3b184819dff60186ad90a71a6965f
Mbianou Bradon <mbianoubradon2000@gmail.com>
2022-12-05 16:49:09 +0100

    Worked on the updateDisplay, deletButton and operation

--- public/main.js
+++ public/main.js
@@ -1,37 +1,24 @@
-class Calculator {
-constructor (previousOperation, currentOperation){
-this.previousOperation = previousOperation;
-this.currentOperation = currentOperation;
+class Computation {
+
+    constructor (previousOperation, currentOperation){
+        this.previousOperation = previousOperation;
+        this.currentOperation = currentOperation;
+    }
+
+  updateDisplay(value){
+    if (previousOperation.innerText.includes('.')) return 
+    previousOperation.innerText = previousOperation.innerText.toString() + value.toString()
+   // currentOperation.innerText = currentOperation.innerText.toString() + value.toString()
+  }
+
+  delete(){
+    previousOperation.innerText = previousOperation.innerText.slice(0, -1)
+    //currentOperation.innerText = currentOperation.innerText.slice(0, -1)
+    
+  }
+
+    
 }
-
-
-delete() {
-
-}
-
-equal(){
-
-}
-
-appendNumber(numbers){
-
-}
-
-chooseOperation(operation)
-
-updateResult(){
-
-}
-
-computation(){
-
-}
-
-
-
-}
-
-
 
 
 
@@ -39,22 +26,35 @@ const numbers = document.querySelectorAll('[data-number]')
 const deleteButton = document.getElementById('delete')
 const equal = document.getElementById('equal')
 const operation = document.querySelectorAll('[data-operation]')
-const previousOperation = document.querySelector('[previous-operation]')
-const currentOperation= document.querySelector('[current-operation]')
+const previousOperation = document.getElementById('previous-operation')
+const currentOperation= document.getElementById('current-operation')
 
 
-numbers.addEventListener('click', ()=>{
-     
+const computation = new Computation (previousOperation, currentOperation)
+
+
+numbers.forEach(button => {
+
+    button.addEventListener('click', ()=>{
+        let value = button.innerText
+        console.log(value);
+        computation.updateDisplay(value);
+    })
+
+})
+
+
+
+operation.forEach(button =>{
+
+    button.addEventListener('click', ()=>{
+     let  operant = button.innerText
+     computation.updateDisplay(operant)
+    })
+
 })
 
 deleteButton.addEventListener ('click', ()=> {
-
-})
-
-equal.addEventListener('click', ()=>{
-
-})
-
-operation.addEventListener('click', ()=>{
-    let operant = operation.innerText;
+    computation.delete()
+    //computation.updateDisplay()
 })
